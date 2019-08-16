1fb7695aca92adac858937ec832c0f585fb02f10
Ramon Chavez <rachavez1974@gmail.com>
2019-08-15 21:29:28 -0400

    Finish activerecord-lifecycle-reading-online-web-sp-000

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get '/posts/:id', to: 'posts#show'
+  get '/posts/:id/edit', to: 'posts#edit', as:'edit_post'
+  patch '/posts/:id', to: 'posts#update', as: 'post'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
